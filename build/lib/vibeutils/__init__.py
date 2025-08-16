1d88db513aededdb16bf1625f254c1babafae073
chuyang <chuyangdeng@gmail.com>
2025-08-15 21:47:33 -0700

    Update .gitignore.

--- build/lib/vibeutils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-vibeutils - A Python library that counts letter frequency using OpenAI API
-"""
-
-from .core import vibecount
-
-__version__ = "0.1.0"
-__author__ = "pika-orac"
-__all__ = ["vibecount"]
